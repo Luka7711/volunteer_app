b141bcf8d87483385c2bfeb445ac41dbee9c015e
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-04-24 23:49:13 -0500

    registration added, needs troubleshooting

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,4 +12,45 @@ router.get('/login', (req, res) => {
 	res.render('users/login.ejs')
 })
 
+
+router.get('/register', (req, res) => {
+
+  const msg = req.session.message
+  req.session.message = ""
+  res.render('users/register.ejs', {
+    message: msg
+  });
+
+})
+
+router.post('/login', (req, res, next) => {
+  res.send(req.body)
+})
+
+router.post('/register', async (req, res, next) => {
+
+  try {
+    const found = await User.findOne({username: req.body.username})
+    console.log(found);
+    if(found !== null) {
+      req.session.message = "Username already taken."
+      res.redirect('/users/register')
+    }
+    else {
+      const createdUser = await User.create(req.body)
+      req.session.loggedIn = true 
+      req.session.username = createdUser.username
+      req.session.message = "Thank you for registering, " + createdUser.username
+      res.redirect('/')
+    }
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+
+
+
 module.exports = router;
